ec581d7d0444130d2b8c87f9b7df23abea298e4f
Dana Sevcik <danasevcik@hotmail.com>
2019-08-28 15:32:33 -0400

    adds navbar component and import react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Route, Switch, withRouter, Link } from "react-router-dom";
+import NavBar from './NavBar.js';
+import About from './About.js';
 import './App.css';
 
 function App() {
   return (
-    <div>
-    </div>
+    <React.Fragment>
+      <div id="navbar">
+        <NavBar/>
+      </div>
+      <Switch>
+        <Route
+          path="/dana"
+          render={routerProps => (
+            <About/>
+          )}/>
+      </Switch>
+    </React.Fragment>
+
   );
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <div>I am navbar</div>
+  )
+
+}
+
+export default NavBar;

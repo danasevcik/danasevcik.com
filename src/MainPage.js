50cbf14b593102ca508fab4e5e05917720fa6103
Dana Sevcik <danasevcik@hotmail.com>
2019-09-09 10:10:36 -0400

    added carousel

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,4 +1,7 @@
 import React, { Fragment, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 function MainPage() {
 
@@ -6,10 +9,15 @@ function MainPage() {
     document.title = 'Dana Sevcik';
   });
 
+
+
   return (
     <Fragment>
-      <div>I am the main page</div>
-      <img src="sunflower.png" alt="sunflower without background"></img>
+      <Carousel showThumbs={false} showArrows={false} showStatus={false} showIndicators={false} infiniteLoop={true} useKeyboardArrows={true} autoPlay={true}>
+        <div style={{height: '100px', color: 'white'}}>I am the main page</div>
+        <div style={{height: '100px', color: 'white'}}>Hello!</div>
+      </Carousel>
+      <img src="sunflower_without_leaves.png" alt="sunflower without background"></img>
     </Fragment>
   )
 

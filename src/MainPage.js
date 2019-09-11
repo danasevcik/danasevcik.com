d2c7677671736dc18dce55604ee8e379a47c5c0d
Dana Sevcik <danasevcik@hotmail.com>
2019-09-11 13:27:50 -0400

    typing component

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,4 +1,6 @@
 import React, { Fragment, useEffect } from 'react';
+import Typing from 'react-typing-animation';
+import Cursor from './Cursor.js'
 
 function MainPage() {
 
@@ -11,8 +13,21 @@ function MainPage() {
       <div id='main-page'>
         <div id='top-line'></div>
         <img src='./headshot_circle.png' id='headshot'/>
-        <div id='name'>Hi, I'm Dana Sevcik!</div>
-        <p id='bottom-line'></p>
+        <p id='name'>Hi, I'm Dana Sevcik!</p>
+        <p id='i-am'>I am a
+          <Typing>
+            <span>Developer</span>
+            <Typing.Backspace count={9} delay={3000} speed={0}/>
+            <Typing.Delay ms={2000}/>
+            <span>Software Engineer</span>
+            <Typing.Backspace count={17} delay={3000} />
+            <Typing.Delay ms={2000}/>
+            <span>Proud Woman In Tech</span>
+            <Typing.Delay ms={100000000000}/>
+          </Typing>
+
+        </p>
+        <div id='bottom-line'></div>
       </div>
     </Fragment>
   )

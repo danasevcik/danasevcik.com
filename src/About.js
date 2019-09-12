07e12ba3d07d917fd52f633ceb895e65d1366190
Dana Sevcik <danasevcik@hotmail.com>
2019-09-12 08:51:13 -0400

    changes z index for nav and menu icon

--- src/About.js
+++ src/About.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import Flickity from 'react-flickity-component'
 
 function About() {
 
@@ -6,12 +7,28 @@ function About() {
     document.title = 'About - Dana Sevcik';
   });
 
+  const flickityOptions = {
+      initialIndex: 2
+  }
+
   return (
     <div id='about-page'>
       <div class='flex'>
         <p id='about-name'>Hi, I'm Dana Sevcik!</p>
       </div>
       <p id='about-text'>This is a bunch of text that i want on the about page. This is a bunch of text that i want on the about page. This is a bunch of text that i want on the about page. This is a bunch of text that i want on the about page. This is a bunch of text that i want on the about page.</p>
+        <Flickity
+          className={'carousel'}
+          elementType={'div'}
+          options={flickityOptions}
+          disableImagesLoaded={false}
+          reloadOnUpdate
+          static
+        >
+          <img src="/images/placeholder.png"/>
+          <img src="/images/placeholder.png"/>
+          <img src="/images/placeholder.png"/>
+        </Flickity>
     </div>
   )
 
